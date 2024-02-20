3dc7a5b7fcb88bac9f8a650ba0d4ea2aff5458d7
huyhoangvn <47599913+huyhoangvn@users.noreply.github.com>
2024-02-21 00:19:24 +0700

    login-admin

--- routes/web/index.js
+++ routes/web/index.js
@@ -1,22 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var AdminCtrl = require("../../controllers/admin/admin-controller");
+var AdminCtrl = require("../../controllers/admin/admin-controller");
+const passport = require('passport')
+const passportConfig = require('../../config/auth/jwt-decode-khachhang')
+const sessionAdmin = require('../../config/auth/session-admin')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.render('index', {title: "Welcome Page"})
-});
-
+router.get('/', AdminCtrl.getViewDangNhapWeb);
 router.use('/auth', require('./auth'))
-router.use('/khachhang', require('./khachhang'))
-router.use('/nhanvien', require('./nhanvien'))
-router.use('/cuahang', require('./cuahang'))
-router.use('/hoadon', require('./hoadon'))
-router.use('/datmon', require('./datmon'))
-router.use('/giohang', require('./giohang'))
-router.use('/danhgia', require('./danhgia'))
-router.use('/khuyenmai', require('./khuyenmai'))
-router.use('/loaimon', require('./loaimon'))
+router.use('/khach-hang', require('./khachhang'))
+router.use('/nhan-vien', require('./nhanvien'))
+router.use('/cua-hang', require('./cuahang'))
+router.use('/hoa-don', require('./hoadon'))
+router.use('/dat-mon', require('./datmon'))
+router.use('/gio-hang', require('./giohang'))
+router.use('/danh-gia', require('./danhgia'))
+router.use('/khuyen-mai', require('./khuyenmai'))
+router.use('/loai-mon', require('./loaimon'))
 router.use('/mon', require('./mon'))
-router.use('/hinhanh', require('./hinhanhmon'))
+router.use('/hinh-anh-mon', require('./hinhanhmon'))
+router.use('/thong-ke', require('./thongke'))
 
 module.exports = router;
