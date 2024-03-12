e5a4d3dd63ffd64657885fcfe0d40f107830b9fa
phucnxph29170 <kingofwar2062003@gmail.com>
2024-03-12 18:23:21 +0700

    fix/tim-kiem-mon-webview

--- controllers/mon/mon-web-controller.js
+++ controllers/mon/mon-web-controller.js
@@ -3,12 +3,16 @@ const { model: Mon } = require("../../model/Mon");
 const { model: CuaHang } = require("../../model/CuaHang");
 const { model: LoaiMon } = require("../../model/LoaiMon");
 const mongo = require('mongoose');
-const { gettatcamonapi } = require("./mon-controller");
+const { getTatCaMon } = require("./mon-controller");
 
 
 const getList =  async (req, res)=>{
     try {
-       
+        const trang = parseInt(req.query.trang) || 1;
+        const soMonTrenTrang = 8; 
+        const soLuongMon = await Mon.countDocuments({});
+        const totalPages = Math.ceil(soLuongMon / soMonTrenTrang);
+
         if(req.query.giaTien === 1){
             req.query.giaTienMax = 100000
             req.query.giaTienMin = 0
@@ -16,7 +20,7 @@ const getList =  async (req, res)=>{
             req.query.giaTienMax = 99999999
             req.query.giaTienMin = 0
         }
-        const result = await gettatcamonapi(req,res);
+        const result = await getTatCaMon(req,res);
         res.render("mon/danh-sach", {
             data: result.list,
             admin: req.session.ten,
