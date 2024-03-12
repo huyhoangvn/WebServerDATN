0830b1dc63b10b5c56487e6238f15063c563f1fa
phucnxph29170 <kingofwar2062003@gmail.com>
2024-03-12 18:04:55 +0700

    fix/hien-thi

--- controllers/mon/mon-web-controller.js
+++ controllers/mon/mon-web-controller.js
@@ -3,65 +3,24 @@ const { model: Mon } = require("../../model/Mon");
 const { model: CuaHang } = require("../../model/CuaHang");
 const { model: LoaiMon } = require("../../model/LoaiMon");
 const mongo = require('mongoose');
+const { gettatcamonapi } = require("./mon-controller");
 
 
 const getList =  async (req, res)=>{
     try {
-        const tenMon = req.query.tenMon != "" ? "^" + req.query.tenMon : "\\w+";
-        const tenCH = req.query.tenCH != "" ? "^" + req.query.tenCH : "\\w+";
-        const tenLM = req.query.tenLM != "" ? "^" + req.query.tenLM : "\\w+";
-        const giaTien = req.query.giaTien != -1 ? req.query.giaTien : Array.from({ length: 10000 }, (_, index) => index - 1);
-        const trangThai = req.query.trangThai;
-
-        // const tenMon = req.query.tenMon || "";
-        // const tenCH = req.query.tenCH || "";
-        // const tenLM = req.query.tenLM || "";
-        // const giaTien = req.query.giaTien !== "-1" ? parseInt(req.query.giaTien) : -1;
-        // const trangThai = req.query.trangThai !== "-1" ? JSON.parse(req.query.trangThai) : -1;
-
-        // const query = {
-        //     // Dùng $regex để kiểm tra giá trị có chứa hay không, tương tự như "LIKE" trong SQL
-        //     tenMon: tenMon !== "" ? { $regex: new RegExp(tenMon, "i") } : { $exists: true },
-        //     tenCH: tenCH !== "" ? { $regex: new RegExp(tenCH, "i") } : { $exists: true },
-        //     tenLM: tenLM !== "" ? { $regex: new RegExp(tenLM, "i") } : { $exists: true },
-        //     giaTien: giaTien !== -1 ? giaTien : { $exists: true },
-        //     trangThai: trangThai !== -1 ? trangThai : { $exists: true },
-        // };
-
-        const trang = parseInt(req.query.trang) || 1;
-        const soMonTrenTrang = 8; 
-        const soLuongMon = await Mon.countDocuments({});
-        const totalPages = Math.ceil(soLuongMon / soMonTrenTrang);
-
-        const data1 = await Mon.find({})
-        .skip((trang - 1) * soMonTrenTrang)
-        .limit(soMonTrenTrang);
-
-
-
-        const allData = [];
-        for(var i = 0; i<data1.length; i++){
-            const idCH = new mongo.Types.ObjectId(data1[i].idCH);
-            const idLM = new mongo.Types.ObjectId(data1[i].idLM);
-
-            const data2 = await LoaiMon.find({_id:idLM});
-            const data3 = await CuaHang.find({_id:idCH});
-
-            const monData = {
-                tenMon: data1[i].tenMon,
-                giaTien: data1[i].giaTien,
-                trangThai: data1[i].trangThai,
-                loaiMon: data2 ? data2[0].tenLM : '',
-                cuaHang: data3 ? data3[0].tenCH : '', 
-            };
-
-            allData.push(monData);
+       
+        if(req.query.giaTien === 1){
+            req.query.giaTienMax = 100000
+            req.query.giaTienMin = 0
+        } else{
+            req.query.giaTienMax = 99999999
+            req.query.giaTienMin = 0
         }
-
+        const result = await gettatcamonapi(req,res);
         res.render("mon/danh-sach", {
-            data: allData,
+            data: result.list,
             admin: req.session.ten,
-            msg: "",
+            msg: result.list,
             totalPages: totalPages,
             currentPage: trang,
         });
