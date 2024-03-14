cc5b1533a8d82164e611cf2136d3d110e193af3a
ngocvu1 <dinhvanvu10a1@gmail.com>
2024-03-14 16:48:42 +0700

    update Quản trị khách hàng

--- controllers/khachhang/khach-hang-web-controller.js
+++ controllers/khachhang/khach-hang-web-controller.js
@@ -1,48 +1,29 @@
 //Website
 const { model: KhachHang } = require("../../model/KhachHang");
 const mongo = require('mongoose');
+const { getKhachHangTheoTen, getSoLuongKhachHang } = require('../../controllers/khachhang/khachhang-controller');
 
 const getList = async (req, res) => {
     try {
         const trang = parseInt(req.query.trang) || 1;
-        const soKhachHangTrenTrang = 8;
-        const soLuongKhachHang = await KhachHang.countDocuments({});
-        const totalPages = Math.ceil(soLuongKhachHang / soKhachHangTrenTrang);
-        const data1 = await KhachHang.find({})
-            .skip((trang - 1) * soKhachHangTrenTrang)
-            .limit(soKhachHangTrenTrang);
-
-
-        const allData = [];
-        for (var i = 0; i < data1.length; i++) {
-
-            const khachHangData = {
-                tenKH: data1[i].tenKH,
-                gioiTinh: data1[i].gioiTinh,
-                taiKhoan: data1[i].taiKhoan,
-                trangThai: data1[i].trangThai,
-                sdt: data1[i].sdt,
-                matKhau: data1[i].matKhau,
-                hinhAnh: data1[i].hinhAnh,
-                diaChi: data1[i].diaChi,
-
-            };
-
-            allData.push(khachHangData);
-        }
-
+        const soKhachHangTrenTrang = 10;
+        const soLuongKhachHang = await getSoLuongKhachHang(req, res);
+        const totalPages = Math.ceil(soLuongKhachHang.count / soKhachHangTrenTrang);
+        const result = await getKhachHangTheoTen(req, res);
         res.render("khachhang/danh-sach", {
-            data: allData,
+            data: result.list,
             admin: req.session.ten,
-            msg: "",
+            msg: result.list,
             totalPages: totalPages,
             currentPage: trang,
         });
 
     } catch (error) {
         console.error("Error fetching data:", error);
+
     }
 }
+
 
 module.exports = {
     getList
