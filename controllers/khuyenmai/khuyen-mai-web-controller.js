039fdff43c7db802bd26f1bc5cf925b18923dbcc
ngocvu1 <dinhvanvu10a1@gmail.com>
2024-03-16 13:49:43 +0700

    khuyen-mai
    quên chưa tạo nhánh khuyến mãi

--- controllers/khuyenmai/khuyen-mai-web-controller.js
+++ controllers/khuyenmai/khuyen-mai-web-controller.js
@@ -1,12 +1,28 @@
 //Website
-const getList =  async (req, res, next)=>{
-    res.render("khuyenmai/danh-sach", {
-        admin: req.session.ten,
-        msg: ""
-    })
+
+const { getTatCaKhuyenMai } = require('../../controllers/khuyenmai/khuyenmai-controller');
+const getList = async (req, res) => {
+    try {
+        const trang = parseInt(req.query.trang) || 1;
+        const soLuongKhuyenMaiTrenTrang = 10;
+        const soLuongKhuyenMai = await getTatCaKhuyenMai(req, res);
+        const totalPages = Math.ceil(soLuongKhuyenMai.count / soLuongKhuyenMaiTrenTrang);
+        const result = await getTatCaKhuyenMai(req, res);
+        res.render("khuyenmai/danh-sach", {
+            data: result.list,
+            admin: req.session.ten,
+            msg: result.list,
+            totalPages: totalPages,
+            currentPage: trang,
+        });
+
+    } catch (error) {
+        console.error("Error fetching data:", error);
+
+    }
 }
 
-const getAdd =  async (req, res, next)=>{
+const getAdd = async (req, res, next) => {
     res.render("khuyenmai/them-moi", {
         admin: req.session.ten,
         msg: ""
