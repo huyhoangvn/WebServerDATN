6c0a165c4ffb9b3de140296fbf1ddaa2bc06af64
ngocvu1 <dinhvanvu10a1@gmail.com>
2024-04-04 09:58:56 +0700

    routing khachHang
    tạm thời

--- routes/api/khachang/datmon.js
+++ routes/api/khachang/datmon.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const datmonCtrl = require('../../../controllers/datmon/datmon-controller')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/them/:idHD', datmonCtrl.addMonDatApi);
+// get mon dat theo id hoa don 
+router.get('/:id', datmonCtrl.getDanhSachMonDatByIdHoaDonApi);
+// delete cung
+router.delete('/:id', datmonCtrl.deleteMonDatApi);
+
+//sua 
+router.put('/:id', datmonCtrl.updateMonDatApi);
 
 module.exports = router;
