00e8ac061fd3412741cf1a91166db08eebe7446b
vanquyet16 <a ch email ca bn trn GitHub>
2024-05-01 17:53:44 +0700

    fixx

--- routes/web/slide.js
+++ routes/web/slide.js
@@ -2,15 +2,13 @@ var express = require('express');
 const slideController = require('../../controllers/slide/slide.controller');
 const router = express.Router();
 const MulterConfigs = require("../../config/multer-config");
-const passport = require('passport')
-const passportConfig = require('../../config/auth/jwt-decode-admin')
-const sessionAdmin = require('../../config/auth/session-admin')
+
 /* GET users listing. */
 router
 .get('/', slideController.getList)
 .post('/', MulterConfigs.upload.array('imageSlide', 1), slideController.addImage)
 .post('/update/:id',MulterConfigs.upload.array('imageSlide', 1), slideController.updateSlide)
 .post('/delete/:id',slideController.deleteSlide)
-
+.get("/theo-cua-hang/:idCH",  slideController.getMonCuaCuaHang);
 
 module.exports = router;
